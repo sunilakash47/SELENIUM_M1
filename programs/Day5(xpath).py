c14f5c227d0bc94daa11c7e18e80f05ec3b315a1
sunilakash47 <assignmentsql@gmail.com>
2023-08-17 08:48:39 +0530

    xpath file commiting

---
+++ programs/Day5(xpath).py
@@ -0,0 +1,41 @@
+"""
+8.xpath
+"""
+"""
+*path of an element present in html tree structure is called as xpath.
+*xpath is classified into 2 types,
+1.absolute xpath
+2.relative xpath
+
+1.absolute xpath:
+*****************
+*absolute xpath indicate by "single forward slash(/)".
+*"/" --> traverse from parent to immediate child.
+
+drawback of absolute xpath:
+===========================
+*xpath will be lengthly compratively relative xpath.
+*always we should traverse from parent to immediate child only.
+*to over come this drawback we go relative xpath
+
+2.relative xpath:
+*****************
+*relative xpath indicates by "double forward slash(//)".
+*"//" --> traverse from parent to any of its child.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
