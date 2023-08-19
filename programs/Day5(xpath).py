c03979c008fa77884d14cbe1a3981857fe5d1531
sunilakash47 <assignmentsql@gmail.com>
2023-08-19 08:58:01 +0530

    text function in xpath

--- programs/Day5(xpath).py
+++ programs/Day5(xpath).py
@@ -109,6 +109,87 @@ driver.get("https://www.myntra.com/kids?f=Categories%3AShirts%3A%3AGender%3Aboys
 driver.maximize_window()
 driver.find_element("xpath", "(//h4[@class='atsa-title'])[3]").click()
 """
+############################################################################################################################
+#19-05-2023
+"""
+xpath by text() function:
+*************************
+*if html code consist of text value then we can go xpath by text function.
+*text value is case-senstive.
+*text() function can replace with dot(.).
+
+syntax:
+-------
+//tagname[text()='text_value']
+                (or)
+//tagname[.='text_value']
+
+
+#xpath to inspect men tab in myntra
+(//a[text()='Men'])[1]    #(//a[.='Men'])[1]
+
+#xpath to inspect profile in myntra 
+//span[.='Profile']
+
+#xpath to inspect login/signup in mytra 
+//a[.='login / Signup']
+
+#xpath to inspect stable version of python in selenium.dev
+//a[.='4.11.2 (August 01, 2023)']
+"""
+#ws to click on login in zomato
+"""
+driver = Chrome(executable_path="../drivers/chromedriver.exe")
+driver.get("https://www.zomato.com/bangalore")
+driver.maximize_window()
+driver.find_element("xpath", "//a[.='Log in']").click()
+"""
+
+#ws to automate pharmacy in medplus
+"""
+driver = Chrome(executable_path="../drivers/chromedriver.exe")
+driver.get("https://www.medplusmart.com/")
+driver.maximize_window()
+driver.find_element("xpath", "//h6[.='Pharmacy']").click()
+driver.find_element("xpath", "//a[.='Upload your Prescription']").click()
+driver.find_element("xpath", "//input[@name='mobileNumber']").send_keys("7688663345")
+driver.find_element("xpath", "//button[.='Send OTP']").click()
+"""
+########################################################################################################################
+"""
+xpath with partial dynamic element:
+***********************************
+*a part/portion of an element is keep on changing is called as partial dynamic element.
+*complete element will not keep on changing only a part of an element is keep on changing.
+*to handle this kind of element we go contains() function.
+*when the "attribute-value and text-value is very lengthy" then also we will go contains().
+
+syntax of attribute in conatins() function :
+--------------------------------------------
+//tag-name[contains(attribute-name , 'attribute-value')]
+
+syntax of text in conatins() function :
+---------------------------------------
+//tag-name[contains(. , 'text-value')]
+
+examples:
+=========
+xpath to inspect python stable version in selenium.dev
+//a[contains(.,'4.11.2')]
+"""
+
+"""
+assignment:
+-----------
+launch passport seva application
+https://www.passportindia.gov.in/AppOnlineProject/welcomeLink#
+click on new registration --> enter all details and click on regsiter button
+
+launch spotify application
+https://open.spotify.com/
+click on signup --> enter all details and click on signup
+"""
+
 
 
 
